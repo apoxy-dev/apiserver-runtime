6498a5c39c0a786f452113f6123981bc7c6ce070
Dmitry Ilyevsky <ilyevsky@gmail.com>
2023-11-01 18:04:50 -0700

    builder: Implement rest.SingularNameProvider for DefaultStrategy

--- pkg/builder/rest/strategy.go
+++ pkg/builder/rest/strategy.go
@@ -41,6 +41,7 @@ type Strategy interface {
 	rest.RESTCreateStrategy
 	rest.RESTDeleteStrategy
 	rest.TableConvertor
+	rest.SingularNameProvider
 }
 
 var _ Strategy = DefaultStrategy{}
@@ -166,4 +167,15 @@ func (d DefaultStrategy) WarningsOnCreate(ctx context.Context, obj runtime.Objec
 // WarningsOnUpdate sends warning header on update
 func (d DefaultStrategy) WarningsOnUpdate(ctx context.Context, obj, old runtime.Object) []string {
 	return nil
+}
+
+// GetSingularName implements rest.SingularNameProvider
+func (d DefaultStrategy) GetSingularName() string {
+	if d.Object == nil {
+		return ""
+	}
+	if n, ok := d.Object.(rest.SingularNameProvider); ok {
+		return n.GetSingularName()
+	}
+	return ""
 }
